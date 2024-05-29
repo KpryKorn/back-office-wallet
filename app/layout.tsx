ed995586501e48947d2d22a281a2e46220301f9b
KpryKorn <roffinisacha@gmail.com>
2024-05-29 11:15:25 +0200

    style: update la config tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className + " container antialiased"}>
+        <main className="mx-auto my-6 md:my-12">{children}</main>
+      </body>
     </html>
   );
 }
